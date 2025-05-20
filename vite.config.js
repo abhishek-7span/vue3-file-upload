561cf2ee7826466b3cc6c821c6e4e93a498539cc
Abhishek <abhishek@Abhishek-7Span.local>
2025-05-20 16:09:42 +0530

    [FEAT] Plugin system.

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "FileUploader.vue"),
+      name: "Vue3FileUpload",
+      fileName: (format) => `vue3-file-upload.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
